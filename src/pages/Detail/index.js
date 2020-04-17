2757b7e29726587facf2f3208897672b85788963
Júlio César Bueno <buenojc@outlook.com>
2020-04-17 00:40:51 -0300

    Create Detail page

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,6 +1,92 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text, Image, TouchableOpacity, FlatList } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import styles from "./styles";
+import logoImg from "../../assets/logo.png";
 
 export default function Incidents() {
-  return <View />;
+  const navigation = useNavigation();
+
+  function returnToIncidents() {
+    navigation.navigate("Incidents");
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logoImg} />
+        <TouchableOpacity onPress={returnToIncidents}>
+          <Feather name="arrow-left" color="#e02041" size={30} />
+        </TouchableOpacity>
+      </View>
+
+      <FlatList
+        data={[1]}
+        keyExtractor={(incident) => String(incident)}
+        showsVerticalScrollIndicator={false}
+        renderItem={() => (
+          <View style={styles.cardsContainer}>
+            {/*Incident Card */}
+
+            <View style={styles.card}>
+              <View style={styles.incidentHeader}>
+                <View style={styles.incidentOngName}>
+                  <Text style={styles.incidentFieldTitle}>Caso:</Text>
+                  <Text style={styles.incidentFieldValue}>
+                    Cadelinha atropelada
+                  </Text>
+                </View>
+
+                <View style={styles.incidentTitle}>
+                  <Text style={styles.incidentFieldTitle}>ONG:</Text>
+                  <Text style={styles.incidentFieldValue}>APAD</Text>
+                </View>
+              </View>
+
+              <Text style={styles.incidentFieldTitle}>Descrição:</Text>
+              <Text style={styles.incidentFieldValue}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in
+                vestibulum quam. Maecenas fermentum sagittis leo, non vestibulum
+                nunc consequat sed. Sed aliquam rutrum enim, id venenatis dui
+                lacinia ut. Praesent suscipit sem non auctor aliquet. Praesent
+                sit amet turpis commodo, tincidunt leo vel, suscipit libero.
+                Mauris ornare velit quis ligula malesuada vulputate. Curabitur
+                id urna est. Maecenas mollis consectetur purus vel vehicula.
+              </Text>
+
+              <Text style={styles.incidentFieldTitle}>Valor:</Text>
+              <Text style={styles.incidentFieldValue}>R$ 120,00</Text>
+            </View>
+
+            {/* End of Incident Card */}
+            {/* Help Card */}
+
+            <View style={styles.card}>
+              <Text style={styles.helpTitle}>Salve o dia!</Text>
+              <Text style={styles.helpTitle}>Seja o herói deste caso!</Text>
+              <Text style={styles.helpDescription}>Entre em contato:</Text>
+
+              <View style={styles.btnContainer}>
+                <TouchableOpacity
+                  style={styles.btn}
+                  onPress={returnToIncidents}
+                >
+                  <Text style={styles.btnText}>Whatsapp</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                  style={styles.btn}
+                  onPress={returnToIncidents}
+                >
+                  <Text style={styles.btnText}>E-mail</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+            {/* End of Help Card */}
+          </View>
+        )}
+      />
+    </View>
+  );
 }
---
+++ src/pages/Detail/styles.js
@@ -0,0 +1,79 @@
+import { StyleSheet } from "react-native";
+import Constants from "expo-constants";
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 24,
+    paddingTop: Constants.statusBarHeight + 20,
+  },
+
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 40,
+  },
+
+  card: {
+    backgroundColor: "#fff",
+    padding: 20,
+    marginBottom: 20,
+  },
+
+  incidentHeader: {
+    flexDirection: "row",
+    marginBottom: 15,
+  },
+
+  incidentOngName: {
+    marginRight: 50,
+  },
+
+  incidentFieldTitle: {
+    textTransform: "uppercase",
+    marginBottom: 10,
+    fontSize: 14,
+    color: "#41414d",
+    textTransform: "uppercase",
+    fontWeight: "bold",
+  },
+
+  incidentFieldValue: {
+    marginTop: 8,
+    fontSize: 15,
+    marginBottom: 24,
+    color: "#737380",
+  },
+
+  helpTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+
+  helpDescription: {
+    marginTop: 8,
+    fontSize: 15,
+    marginBottom: 24,
+    marginTop: 20,
+    color: "#737380",
+  },
+
+  btnContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+
+  btn: {
+    backgroundColor: "#e02041",
+    width: "47%",
+    paddingVertical: 15,
+    borderRadius: 10,
+  },
+
+  btnText: {
+    color: "#fff",
+    textAlign: "center",
+  },
+});
